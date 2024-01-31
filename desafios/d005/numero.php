525c0cef9f7af53ecb3904f4a3ecd8cc25c9c4d5
Lucas Evangelista <130062327+lucasevangelistaa@users.noreply.github.com>
2024-01-31 13:13:51 -0300

    criei o desafio 5

---
+++ desafios/d005/numero.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dasafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Analisador de Número Real</h1>
+        <?php 
+            $num = $_POST["num"] ?? 0;
+
+            echo "<p>Analizando o número <strong>". number_format($num, 3, ",", ".") ."</strong> informado pelo usuário: </p>";
+
+            $int = (int) $num;
+            $fra = $num - $int;
+
+            echo "
+                <ul>
+                    <li>A parte inteira do número é <strong>". number_format($int, 0, ",", ".") ."</strong></li>
+                    <li>A parte fracionária do número é <strong>". number_format($fra, 3, ",", ".") ."</strong></li>
+                </ul>
+            ";
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+</body>
+</html>
