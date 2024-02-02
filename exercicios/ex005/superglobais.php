86bad962f3f6fe41dbf18a7d2432a012b119ca35
Lucas Evangelista <130062327+lucasevangelistaa@users.noreply.github.com>
2024-02-02 11:18:49 -0300

    criei o exercicio 5

---
+++ exercicios/ex005/superglobais.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <pre>
+            <?php 
+                setcookie("dia-da-semana", "SEXTA", time() + 3600);
+
+                session_start();
+                $_SESSION["teste"] = "FUNCIONOU!";
+
+                echo "<h1>Superglobal GET</h1>";
+                var_dump($_GET);
+
+                echo "<h1>Superglobal POST</h1>";
+                var_dump($_POST);
+
+                echo "<h1>Superglobal REQUEST</h1>";
+                var_dump($_REQUEST);
+
+                echo "<h1>Superglobal COOKIE</h1>";
+                var_dump($_COOKIE);
+
+                echo "<h1>Superglobal SESSION</h1>";
+                var_dump($_SESSION);
+
+                echo "<h1>Superglobal ENV</h1>";
+                var_dump($_ENV);
+                // foreach (getenv() as $c => $v) {
+                //     echo "<br> $c -> $v";
+                // }
+                
+                echo "<h1>Superglobal SERVER</h1>";
+                var_dump($_SERVER);
+
+                echo "<h1>Superglobal GLOBALS</h1>";
+                var_dump($GLOBALS);
+            ?>
+        </pre>
+    </main>
+</body>
+</html>
